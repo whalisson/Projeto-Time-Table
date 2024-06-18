e0f4a8e05f528cbd0eacb16a625a089d14eece0f
Whalisson <whalisson@hotmail.com>
2024-06-18 19:40:08 -0300

    Nova views

--- mm1/views.py
+++ mm1/views.py
@@ -31,13 +31,14 @@ class Data:
 
 class Schedule:
     def __init__(self):
-        self._data = data
-        self._classes = []
-        self._numberOfConflicts = 0
-        self._fitness = -1
-        self._classNumb = 0
-        self._isFitnessChanged = True
+        self._data = data  # Instância de Data para acessar dados
+        self._classes = []  # Lista de classes no cronograma
+        self._numberOfConflicts = 0  # Contador de conflitos
+        self._fitness = -1  # Valor de fitness
+        self._classNumb = 0  # Contador de classes
+        self._isFitnessChanged = True  # Flag para indicar se o valor de fitness mudou
 
+    # Métodos de acesso
     def get_classes(self):
         self._isFitnessChanged = True
         return self._classes
@@ -46,28 +47,28 @@ class Schedule:
 
     def get_fitness(self):
         if self._isFitnessChanged:
-            self._fitness = self.calculate_fitness()
+            self._fitness = self.calculate_fitness()  # Calcula a fitness se ela mudou
             self._isFitnessChanged = False
         return self._fitness
 
     def initialize(self):
-        sections = Section.objects.all()
+        sections = Section.objects.all()  # Todas as seções
         for section in sections:
-            dept = section.department
-            n = section.num_class_in_week
+            dept = section.department  # Departamento da seção
+            n = section.num_class_in_week  # Número de aulas na semana
             if n <= len(MeetingTime.objects.all()):
-                courses = dept.courses.all()
+                courses = dept.courses.all()  # Cursos do departamento
                 for course in courses:
                     for i in range(n // len(courses)):
-                        crs_inst = course.instructors.all()
-                        newClass = Class(self._classNumb, dept, section.section_id, course)
+                        crs_inst = course.instructors.all()  # Instrutores do curso
+                        newClass = Class(self._classNumb, dept, section.section_id, course)  # Nova classe
                         self._classNumb += 1
                         newClass.set_meetingTime(data.get_meetingTimes()[rnd.randrange(0, len(MeetingTime.objects.all()))])
                         newClass.set_room(data.get_rooms()[rnd.randrange(0, len(data.get_rooms()))])
                         newClass.set_instructor(crs_inst[rnd.randrange(0, len(crs_inst))])
                         self._classes.append(newClass)
             else:
-                n = len(MeetingTime.objects.all())
+                n = len(MeetingTime.objects.all())  # Usa todos os horários disponíveis
                 courses = dept.courses.all()
                 for course in courses:
                     for i in range(n // len(courses)):
@@ -78,115 +79,107 @@ class Schedule:
                         newClass.set_room(data.get_rooms()[rnd.randrange(0, len(data.get_rooms()))])
                         newClass.set_instructor(crs_inst[rnd.randrange(0, len(crs_inst))])
                         self._classes.append(newClass)
-
         return self
 
     def calculate_fitness(self):
-        self._numberOfConflicts = 0
+        self._numberOfConflicts = 0  # Inicializa contagem de conflitos
         classes = self.get_classes()
         for i in range(len(classes)):
-            if classes[i].room.seating_capacity < int(classes[i].course.max_numb_students):
+            if classes[i].room.seating_capacity < int(classes[i].course.max_numb_students):  # Verifica capacidade da sala
                 self._numberOfConflicts += 1
             for j in range(len(classes)):
                 if j >= i:
                     if (classes[i].meeting_time == classes[j].meeting_time) and \
                             (classes[i].section_id != classes[j].section_id) and (classes[i].section == classes[j].section):
-                        if classes[i].room == classes[j].room:
+                        if classes[i].room == classes[j].room:  # Conflito de sala
                             self._numberOfConflicts += 1
-                        if classes[i].instructor == classes[j].instructor:
+                        if classes[i].instructor == classes[j].instructor:  # Conflito de instrutor
                             self._numberOfConflicts += 1
-        return 1 / (1.0 * self._numberOfConflicts + 1)
+        return 1 / (1.0 * self._numberOfConflicts + 1)  # Retorna a fitness inversamente proporcional aos conflitos
 
-
+# Classe para representar uma população de cronogramas
 class Population:
     def __init__(self, size):
-        self._size = size
-        self._data = data
-        self._schedules = [Schedule().initialize() for i in range(size)]
+        self._size = size  # Tamanho da população
+        self._data = data  # Dados da instância Data
+        self._schedules = [Schedule().initialize() for i in range(size)]  # Inicializa a população
 
     def get_schedules(self):
         return self._schedules
 
-
+# Implementação do algoritmo genético
 class GeneticAlgorithm:
     def evolve(self, population):
-        return self._mutate_population(self._crossover_population(population))
+        return self._mutate_population(self._crossover_population(population))  # Realiza cruzamento e mutação
 
     def _crossover_population(self, pop):
-        crossover_pop = Population(0)
+        crossover_pop = Population(0)  # População de cruzamento vazia
         for i in range(NUMB_OF_ELITE_SCHEDULES):
-            crossover_pop.get_schedules().append(pop.get_schedules()[i])
+            crossover_pop.get_schedules().append(pop.get_schedules()[i])  # Mantém os cronogramas elite
         i = NUMB_OF_ELITE_SCHEDULES
         while i < POPULATION_SIZE:
-            schedule1 = self._select_tournament_population(pop).get_schedules()[0]
+            schedule1 = self._select_tournament_population(pop).get_schedules()[0]  # Seleção por torneio
             schedule2 = self._select_tournament_population(pop).get_schedules()[0]
-            crossover_pop.get_schedules().append(self._crossover_schedule(schedule1, schedule2))
+            crossover_pop.get_schedules().append(self._crossover_schedule(schedule1, schedule2))  # Cruza cronogramas
             i += 1
         return crossover_pop
 
     def _mutate_population(self, population):
         for i in range(NUMB_OF_ELITE_SCHEDULES, POPULATION_SIZE):
-            self._mutate_schedule(population.get_schedules()[i])
+            self._mutate_schedule(population.get_schedules()[i])  # Aplica mutação nos cronogramas
         return population
 
     def _crossover_schedule(self, schedule1, schedule2):
-        crossoverSchedule = Schedule().initialize()
+        crossoverSchedule = Schedule().initialize()  # Inicializa um novo cronograma para cruzamento
         for i in range(0, len(crossoverSchedule.get_classes())):
             if rnd.random() > 0.5:
-                crossoverSchedule.get_classes()[i] = schedule1.get_classes()[i]
+                crossoverSchedule.get_classes()[i] = schedule1.get_classes()[i]  # Escolhe classe do primeiro cronograma
             else:
-                crossoverSchedule.get_classes()[i] = schedule2.get_classes()[i]
+                crossoverSchedule.get_classes()[i] = schedule2.get_classes()[i]  # Escolhe classe do segundo cronograma
         return crossoverSchedule
 
     def _mutate_schedule(self, mutateSchedule):
-        schedule = Schedule().initialize()
+        schedule = Schedule().initialize()  # Inicializa novo cronograma para mutação
         for i in range(len(mutateSchedule.get_classes())):
             if MUTATION_RATE > rnd.random():
-                mutateSchedule.get_classes()[i] = schedule.get_classes()[i]
+                mutateSchedule.get_classes()[i] = schedule.get_classes()[i]  # Substitui classes aleatoriamente
         return mutateSchedule
 
     def _select_tournament_population(self, pop):
-        tournament_pop = Population(0)
+        tournament_pop = Population(0)  # População de torneio vazia
         i = 0
         while i < TOURNAMENT_SELECTION_SIZE:
-            tournament_pop.get_schedules().append(pop.get_schedules()[rnd.randrange(0, POPULATION_SIZE)])
+            tournament_pop.get_schedules().append(pop.get_schedules()[rnd.randrange(0, POPULATION_SIZE)])  # Seleção aleatória
             i += 1
-        tournament_pop.get_schedules().sort(key=lambda x: x.get_fitness(), reverse=True)
+        tournament_pop.get_schedules().sort(key=lambda x: x.get_fitness(), reverse=True)  # Ordena pela fitness
         return tournament_pop
 
-
+# Classe que representa uma aula
 class Class:
     def __init__(self, id, dept, section, course):
-        self.section_id = id
-        self.department = dept
-        self.course = course
-        self.instructor = None
-        self.meeting_time = None
-        self.room = None
-        self.section = section
+        self.section_id = id  # ID da seção
+        self.department = dept  # Departamento
+        self.course = course  # Curso
+        self.instructor = None  # Instrutor (a definir)
+        self.meeting_time = None  # Horário (a definir)
+        self.room = None  # Sala (a definir)
+        self.section = section  # Seção
 
+    # Métodos de acesso e configuração
     def get_id(self): return self.section_id
-
     def get_dept(self): return self.department
-
     def get_course(self): return self.course
-
     def get_instructor(self): return self.instructor
-
     def get_meetingTime(self): return self.meeting_time
-
     def get_room(self): return self.room
-
     def set_instructor(self, instructor): self.instructor = instructor
-
     def set_meetingTime(self, meetingTime): self.meeting_time = meetingTime
-
     def set_room(self, room): self.room = room
 
-
+# Instância global de dados
 data = Data()
 
-
+# Função para gerar o contexto do cronograma para renderizar
 def context_manager(schedule):
     classes = schedule.get_classes()
     context = []
@@ -202,11 +195,11 @@ def context_manager(schedule):
         context.append(cls)
     return context
 
-
+# Função para renderizar a página inicial
 def home(request):
     return render(request, 'index.html', {})
 
-
+# Função para gerar e renderizar o horário
 def timetable(request):
     schedule = []
     population = Population(POPULATION_SIZE)
@@ -223,8 +216,7 @@ def timetable(request):
     return render(request, 'timetable.html', {'schedule': schedule, 'sections': Section.objects.all(),
                                               'times': MeetingTime.objects.all()})
 
-
-
+# Função para adicionar instrutor
 def add_instructor(request):
     form = InstructorForm(request.POST or None)
     if request.method == 'POST':
@@ -236,21 +228,21 @@ def add_instructor(request):
     }
     return render(request, 'adins.html', context)
 
-
+# Função para listar instrutores
 def inst_list_view(request):
     context = {
         'instructors': Instructor.objects.all()
     }
     return render(request, 'instlist.html', context)
 
-
+# Função para deletar instrutor
 def delete_instructor(request, pk):
     inst = Instructor.objects.filter(pk=pk)
     if request.method == 'POST':
         inst.delete()
         return redirect('editinstructor')
 
-
+# Função para adicionar sala
 def add_room(request):
     form = RoomForm(request.POST or None)
     if request.method == 'POST':
@@ -262,28 +254,28 @@ def add_room(request):
     }
     return render(request, 'addrm.html', context)
 
-
+# Função para listar salas
 def room_list(request):
     context = {
         'rooms': Room.objects.all()
     }
     return render(request, 'rmlist.html', context)
 
-
+# Função para deletar sala
 def delete_room(request, pk):
     rm = Room.objects.filter(pk=pk)
     if request.method == 'POST':
         rm.delete()
         return redirect('editrooms')
 
-
+# Função para listar horários
 def meeting_list_view(request):
     context = {
         'meeting_times': MeetingTime.objects.all()
     }
     return render(request, 'mtlist.html', context)
 
-
+# Função para adicionar horário
 def add_meeting_time(request):
     form = MeetingTimeForm(request.POST or None)
     if request.method == 'POST':
@@ -297,21 +289,21 @@ def add_meeting_time(request):
     }
     return render(request, 'addmt.html', context)
 
-
+# Função para deletar horário
 def delete_meeting_time(request, pk):
     mt = MeetingTime.objects.filter(pk=pk)
     if request.method == 'POST':
         mt.delete()
         return redirect('editmeetingtime')
 
-
+# Função para listar cursos
 def course_list_view(request):
     context = {
         'courses': Course.objects.all()
     }
     return render(request, 'crslist.html', context)
 
-
+# Função para adicionar curso
 def add_course(request):
     form = CourseForm(request.POST or None)
     if request.method == 'POST':
@@ -325,14 +317,14 @@ def add_course(request):
     }
     return render(request, 'adcrs.html', context)
 
-
+# Função para deletar curso
 def delete_course(request, pk):
     crs = Course.objects.filter(pk=pk)
     if request.method == 'POST':
         crs.delete()
         return redirect('editcourse')
 
-
+# Função para adicionar departamento
 def add_department(request):
     form = DepartmentForm(request.POST or None)
     if request.method == 'POST':
@@ -344,21 +336,21 @@ def add_department(request):
     }
     return render(request, 'addep.html', context)
 
-
+# Função para listar departamentos
 def department_list(request):
     context = {
         'departments': Department.objects.all()
     }
     return render(request, 'deptlist.html', context)
 
-
+# Função para deletar departamento
 def delete_department(request, pk):
     dept = Department.objects.filter(pk=pk)
     if request.method == 'POST':
         dept.delete()
         return redirect('editdepartment')
 
-
+# Função para adicionar seção
 def add_section(request):
     form = SectionForm(request.POST or None)
     if request.method == 'POST':
@@ -370,14 +362,14 @@ def add_section(request):
     }
     return render(request, 'addsec.html', context)
 
-
+# Função para listar seções
 def section_list(request):
     context = {
         'sections': Section.objects.all()
     }
     return render(request, 'seclist.html', context)
 
-
+# Função para deletar seção
 def delete_section(request, pk):
     sec = Section.objects.filter(pk=pk)
     if request.method == 'POST':
